226b3c0da7ca1458f2c816727436e9b60a351b65
aistonija <aistonija16@gmail.com>
2021-06-29 11:04:38 +0300

    switch fruits task

--- 4_week/06-29/2_conditionals/script.js
+++ 4_week/06-29/2_conditionals/script.js
@@ -24,17 +24,65 @@ let result =
 Pasirašyk kintamą su savo vardu. Sukurk if-else, kuris pasakys "Ilgas vardas", jei tavo vardas ilgesnis nei 6 raidės. Kitu atveju, nieko neišmes. String ilgį gali susižinoti prirašant 'length' property (pvz.: "Petras".length arba su kintamu: name.length).
 */
 
-let myName = prompt("Ivesk varda");
+// let myName = prompt("Ivesk varda");
 
-if (myName !== "") {
-  let prettyName =
-    myName.charAt(0).toUpperCase() + myName.substr(1).toLowerCase();
+// if (myName !== "") {
+//   let prettyName =
+//     myName.charAt(0).toUpperCase() + myName.substr(1).toLowerCase();
 
-  console.log(
-    prettyName.length > 6
-      ? `vardas ${prettyName} ilgas 🌴`
-      : `vardas ${prettyName} trumpas 🌺`
-  );
-} else {
-  console.log("Neivedete vardo 😪");
+//   console.log(
+//     prettyName.length > 6
+//       ? `vardas ${prettyName} ilgas 🌴`
+//       : `vardas ${prettyName} trumpas 🌺`
+//   );
+// } else {
+//   console.log("Neivedete vardo 😪");
+// }
+
+// SWITCH
+
+let x = "0";
+let text;
+
+switch (x) {
+  case 0:
+    text = "Off";
+    break;
+  case 1:
+    text = "On";
+    break;
+  default:
+    text = "No value found";
 }
+
+// console.log(text);
+
+/*
+Sukurkite variable 'userInput' ir pradžioje prilyginkite 'Obuolys'. Su Switch statement apsirašyk, kad pasakytų ar vartotojo įvestas laukelis - vaisius ar daržovė. Pridėkite bent 5 kiekvienos kategorijos
+*/
+
+// let userInput = "Obuolys";
+let userInput = prompt("Iveskite vaisiu arba darzove");
+
+let switchResult;
+
+switch (userInput) {
+  case "Obuolys":
+  case "Kriaušė":
+  case "Persikas":
+  case "Bananas":
+  case "Citrina":
+    switchResult = "Vaisius";
+    break;
+  case "Bulvė":
+  case "Morka":
+  case "Kopūstas":
+  case "Svogūnas":
+  case "Agurkas":
+    switchResult = "Daržovė";
+    break;
+  default:
+    switchResult = "Neaišku";
+}
+
+console.log(switchResult);
