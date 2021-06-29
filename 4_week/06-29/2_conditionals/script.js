bd44381139c0d2e1ff5ddcc7f70854afba86a0dd
aistonija <aistonija16@gmail.com>
2021-06-29 10:36:13 +0300

    name.length task

--- 4_week/06-29/2_conditionals/script.js
+++ 4_week/06-29/2_conditionals/script.js
@@ -6,16 +6,35 @@ Sukurk variable 'legalAge', kuris būtų lygus 20. Antra variable 'clientAge' su
 */
 
 let legalAge = 20;
-let clientAge = Number(prompt("Iveskite amziu"));
+let clientAge = 15;
 
-if (clientAge >= legalAge) {
-  console.log("Ileidziam, pakankamai suauges");
-} else {
-  console.log("NEileidziam, per jaunas");
-}
+// if (clientAge >= legalAge) {
+//   console.log("Ileidziam, pakankamai suauges");
+// } else {
+//   console.log("NEileidziam, per jaunas");
+// }
 
 let result =
   clientAge >= legalAge
     ? "Ileidziam, pakankamai suauges"
     : "NEileidziam, per jaunas";
-console.log(result);
+// console.log(result);
+
+/*
+Pasirašyk kintamą su savo vardu. Sukurk if-else, kuris pasakys "Ilgas vardas", jei tavo vardas ilgesnis nei 6 raidės. Kitu atveju, nieko neišmes. String ilgį gali susižinoti prirašant 'length' property (pvz.: "Petras".length arba su kintamu: name.length).
+*/
+
+let myName = prompt("Ivesk varda");
+
+if (myName !== "") {
+  let prettyName =
+    myName.charAt(0).toUpperCase() + myName.substr(1).toLowerCase();
+
+  console.log(
+    prettyName.length > 6
+      ? `vardas ${prettyName} ilgas 🌴`
+      : `vardas ${prettyName} trumpas 🌺`
+  );
+} else {
+  console.log("Neivedete vardo 😪");
+}
