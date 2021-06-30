ac8838ddad23c87da0828783928827fe8eb9f606
aistonija <aistonija16@gmail.com>
2021-06-30 10:59:19 +0300

    flex items baxkground color change on click

--- 4_week/06-30/2_flex-cont-events/script.js
+++ 4_week/06-30/2_flex-cont-events/script.js
@@ -2,19 +2,45 @@
 
 // elements
 const flexContainer = document.querySelector(".flex-container");
+const flexItems = document.querySelectorAll(".flex-item");
 
 // events
 // Kuomet pele uzvedama ant elemento, uzdedame outline (event)
-flexContainer.addEventListener("mouseenter", addOutline);
+flexContainer.addEventListener("mouseenter", function () {
+  flexContainer.style.outline = "6px solid hotpink";
+});
 
-// Kuomet pele patraukiama nuo elemento, outline nuimame
+// // Kuomet pele patraukiama nuo elemento, outline nuimame
 flexContainer.addEventListener("mouseleave", removeOutline);
 
-// functions
-function addOutline() {
-  flexContainer.style.outline = "6px solid hotpink";
+// Sukame cikla per visus flex-items, kad galetume identifikuoti, kiekviena atskirai
+
+for (let i = 0; i < flexItems.length; i++) {
+  let square = flexItems[i];
+  //   console.log(i, square);
+
+  square.addEventListener("mouseenter", function () {
+    square.style.outline = "4px solid orange";
+  });
+
+  square.addEventListener("mouseleave", function () {
+    square.style.outline = "none";
+  });
+
+  square.addEventListener("click", function () {
+    square.style.backgroundColor = `rgb(${randomInt()}, ${randomInt()}, ${randomInt()})`;
+    // pamastyti/pasirasyti if salyga, kad:
+    // paspaudus peles klavisa spalva uzsideda, arba nusiima.
+  });
 }
 
+// functions
 function removeOutline() {
   flexContainer.style.outline = "none";
+}
+
+// helper function
+// background-color: rgb(15,85,0)
+function randomInt() {
+  return Math.floor(Math.random() * 256);
 }
